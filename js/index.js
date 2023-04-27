3494d4d09c144005b456d124ec01b2fdc4270b64
sokari <patsoks.sokari@gmail.com>
2023-04-28 00:56:54 +0100

    add animation to all pages

--- js/index.js
+++ js/index.js
@@ -2,7 +2,18 @@ const menuBtn = document.querySelector('#menu-spinner');
 const menuBar = document.querySelector('.topnav');
 
 menuBtn.addEventListener('click', () => {
-    if (menuBar.style.display == '' || menuBar.style.display == 'none')  
+    if (menuBar.style.display == '' || menuBar.style.display == 'none')
+    {
         menuBar.style.display= 'flex';
+        menuBar.animate([
+            {transform: "translateX(100px)"},
+            {transform: "translateX(0px)"},
+        ],
+        {
+            duration: 200,
+            iterations: 1,
+            easing: 'ease-out',
+        });
+    }  
     else menuBar.style.display = 'none';
 }, false)
