b624e09b8634976e5d193d849966924fed3b4b9a
sokari <patsoks.sokari@gmail.com>
2023-04-28 12:40:06 +0100

    finish the second animation for a closed menu icon

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector('#menu-spinner');
 const menuBar = document.querySelector('.topnav');
+const menuBtnOn = document.querySelector('#menu-spinner-on');
+
+console.log(menuBtnOn);
 
 menuBtn.addEventListener('click', () => {
     if (menuBar.style.display == '' || menuBar.style.display == 'none')
@@ -14,6 +17,17 @@ menuBtn.addEventListener('click', () => {
             iterations: 1,
             easing: 'ease-out',
         });
+        menuBar.style.display == 'none';
+        menuBtnOn.style.display = 'block';
     }  
-    else menuBar.style.display = 'none';
-}, false)
+    else {
+        menuBar.style.display = 'none';
+        menuBtnOn.style.display = 'none';
+    }
+}, false);
+
+menuBtnOn.addEventListener('click', () => {
+    menuBar.style.display = 'none';
+    menuBtnOn.style.display = 'none';
+    menuBar.style.display == 'block';
+}, false);
